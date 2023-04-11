1906e69578d21731a991ddb4d00d9bf51f9173a8
smenaq <sergiosantosmenaq@gmail.com>
2023-04-11 18:20:47 -0500

    check inputs done

--- app/main.js
+++ app/main.js
@@ -1,7 +1,22 @@
 const $ = selector => document.querySelector(selector)
 
+const checkInput = event => {
+  const regex = /^[a-z]*$/;
+  const key = String.fromCharCode(event.keyCode);
+  if (!regex.test(key)) {
+    if(key === ' ' || key === '\r') return true
+    event.preventDefault();
+    return false;
+  }
+  return true;
+}
+
 $('#encriptar').addEventListener('click', e => {
   const { value } = $('#inputText')
   value.split('\n').forEach(paragraph => $('#resultText').innerHTML += paragraph + '<br>')
 
+})
+
+$('#inputText').addEventListener('keypress', e => {
+  checkInput(e)
 })
